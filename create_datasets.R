e5448db481520e40f206d490724392a6b3612f30
Allison Hui <alh322@cornell.edu>
2024-05-04 14:09:30 -0400

    changed colnames to upper, converted date to date format, cleaned data, fixed station_name

--- create_datasets.R
+++ create_datasets.R
@@ -6,22 +6,25 @@ fnames
 
 # read in the first dataset to initialize a data frame
 dat <- read.table( fnames[1], header = FALSE)
-filename<- basename(fnames[j])
+filename<- basename(fnames[1])
 state<- str_extract(filename, "(?<=-)[A-Z]{2}" )
-station_name<- str_extract(filename, "(?<=-)[A-Z]{2}(?=_SSE\\.txt$)") #doesn't work
+#filename <- "CRND0103-2013-AL_Gainesville_2_NE.txt"
+station_name<- str_extract(filename, "(?<=-[A-Z]{2}_)([A-Za-z0-9_]+)(?=\\.txt)") #doesn't work
+station_name <- str_replace_all(station_name, "_", " ")
 dat$state<- state
 dat$station_name<- station_name
 
 
 # loop over the rest of the files and rbind them
-for(j in 2:length(fnames)){
-  df<- read.table( fnames[j] , header = FALSE )
-  filename<- basename(fnames[j])
-  state<- str_extract(filename, "(?<=-)[A-Z]{2}" )
-  station_name<- str_extract(filename, "(?<=-)[A-Za-z0-9_]+(?=_SSE\\.txt$)")
-  rbind(dat, df)
-  
+for (j in 2:length(fnames)) {
+  df <- read.table(fnames[j], header = FALSE)
+  filename <- basename(fnames[j])
+  df$state <- str_extract(filename, "(?<=-)[A-Z]{2}")
+  df$station_name <- str_extract(filename, "(?<=-[A-Z]{2}_)([A-Za-z0-9_]+)(?=\\.txt)")
+  df$station_name <- str_replace_all(station_name, "_", " ")
+  dat <- rbind(dat, df)  
 }
+
 #set column names
 colnames(dat)<-c("wbanno","lst_date","crx_vn","longitude","latitude","t_daily_max",
                              "t_daily_min","t_daily_mean","t_daily_avg","p_daily_calc",
@@ -31,3 +34,10 @@ colnames(dat)<-c("wbanno","lst_date","crx_vn","longitude","latitude","t_daily_ma
                              "soil_moisture_50", "soil_moisture_100", "soil_temp_5",
                              "soil_temp_10", "soil_temp_20", "soil_temp_50", "soil_temp_100","state",
                  "station_name")
+colnames(dat) <- toupper(colnames(dat))
+
+#convert date
+dat$LST_DATE <- as.Date(as.character(dat$LST_DATE), format = "%Y%m%d")
+#Clean data
+dat[dat == -9999] <- NA
+

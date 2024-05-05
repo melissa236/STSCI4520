0caadf499b99f8627432850c5eea53f2f363d453
melissa236 <mae98@cornell.edu>
2024-05-04 20:54:26 -0400

    added comments

--- climate.usa/R/create_grid.R
+++ climate.usa/R/create_grid.R
@@ -3,12 +3,14 @@ library(ggplot2)
 library(maps)
 
 create_usa_grid<- function(resolution = 1){
-  longitude<- dat$LONGITUDE
-  latitude<- dat$LATITUDE
+  
+  #map data for contiguous USA
   usa_map<- map("usa", fill = FALSE, plot = FALSE)
   usa_polygon<- usa_map$range
+  #extract longitude and latitude
   x_range<- seq(usa_polygon[1], usa_polygon[2], by = resolution)
   y_range<- seq(usa_polygon[3], usa_polygon[4], by = resolution)
+  #generate grid points
   grid_points<- expand.grid(LONGITUDE = x_range, LATITUDE = y_range)
   
   return(grid_points)

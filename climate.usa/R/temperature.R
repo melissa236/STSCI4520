8627379f4678e9906b056a1761a055cfb499e9fa
Allison Hui <alh322@cornell.edu>
2024-05-07 19:45:20 -0400

    docs

--- climate.usa/R/temperature.R
+++ climate.usa/R/temperature.R
@@ -1,19 +1,27 @@
+#' Estimate trend in temperature
 #'
+#' Estimate the trend of temperatures over time, in units of degrees Celsius per year.
 #'
+#' @param data contains the created dataset
+#' @param data_column is date column in data
+#' @param temp_column is temperature column in data
 #'
-#'
-#'
-#'
+#' @return a plot with
+#' \itemize{
+#' \item \code{type} curve of the temperature trend
+#' }
+#'@examples
+#'temp_trend(dat, "LST_DATE", "T_DAILY_AVG")
 
 
-temp_trend <- function(data, date_column, temp_column, degree = 2) {
+temp_trend <- function(data, date_column, temp_column) {
 
   data$YEAR <- year(data[[date_column]])
   yearly_data <- data %>%
     group_by(YEAR) %>%
     summarize(AVG_TEMP = mean(.data[[temp_column]], na.rm = TRUE), .groups = 'drop')
 
-  model_formula <- as.formula(paste("AVG_TEMP ~ poly(YEAR, ", degree, ")"))
+  model_formula <- as.formula(paste("AVG_TEMP ~ poly(YEAR, ", 2, ")"))
   model <- lm(model_formula, data = yearly_data)
 
   prediction_years <- data.frame(YEAR = seq(min(yearly_data$YEAR), max(yearly_data$YEAR), length.out = 200))
@@ -26,7 +34,7 @@ temp_trend <- function(data, date_column, temp_column, degree = 2) {
     theme_minimal()
 }
 
-#temp_trend(dat, "LST_DATE", "T_DAILY_AVG", 2)
+#temp_trend(dat, "LST_DATE", "T_DAILY_AVG")
 
 
 
--- climate.usa/R/time.R
+++ climate.usa/R/time.R
@@ -1,12 +1,21 @@
+#' Eextract the time series
 #'
+#' Eextract the time series for a specific station by station id.
 #'
+#' @param name station name contains the city name, station number, and station direction
+#' @param starting_date date in date format of range you want to start with
+#' @param ending_date date in date format of range you want to end with
 #'
-#'
-#'
-#'
-#'
-#'
-#'
+#' @return a data frame with the following components:
+#' \itemize{
+#' \item \code{type} data for the dates in selected ranges
+#' }
+#'@examples
+#' data for Asheville 13 S since January 3 2005
+#' print(get_time_series("Asheville 13 S", "2005-01-03"))
+#' data for Asheville 13 S since January 3 2006 until January 3 2009
+#' print(get_time_series("Asheville 13 S", "2006-01-03", "2009-01-03"))
+#' @export
 library(dplyr)
 get_time_series <- function(name, starting_date = NULL, ending_date = NULL) {
   station_data <- dat %>%
